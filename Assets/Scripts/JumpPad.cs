9ba1ecf614337c7e3686de03aae594eaa3b446e6
Bennet Meier <bennet.meierqgmx.net>
2020-02-11 22:17:09 +0100

    New Level and Adjustments

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -6,8 +6,12 @@ public class JumpPad : MonoBehaviour
 {
     [SerializeField] private float _force = 0;
     [SerializeField] private Vector2 _angle = Vector2.zero;
+    [SerializeField] private bool _transporter = false;
+    [SerializeField] private float _offTime = 0;
  
     private Rigidbody2D _rig = null;
+
+    private PlayerController _player = null;
 
     void Start()
     {
@@ -20,7 +24,20 @@ public class JumpPad : MonoBehaviour
         if (collision.gameObject.GetComponent<PlayerController>())
         {
             _rig = collision.gameObject.GetComponent<Rigidbody2D>();
+            if (_transporter)
+            {
+                _player = collision.gameObject.GetComponent<PlayerController>();
+                _player.enabled = false;
+                StartCoroutine(OfftimeRoutine());
+            }
+            _rig.velocity = Vector2.zero;
             _rig.AddForce(_angle * _force);
         }
+    }
+
+    private IEnumerator OfftimeRoutine()
+    {
+        yield return new WaitForSeconds(_offTime);
+        _player.enabled = true;
     }
 }
