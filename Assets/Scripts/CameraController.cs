b493a1e9261cc16a49bfadce920a936256cb7a9e
Bennet Meier <bennet.meierqgmx.net>
2020-02-23 12:53:02 +0100

    Pathfinding

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera _vCam = null;
+    [SerializeField] private float _shakeFreq = 0;
+    [SerializeField] private float _shakeAmp = 0;
+
+    [SerializeField] private CinemachineCameraOffset _offset = null;
+    private CinemachineBasicMultiChannelPerlin _noise = null;
+
+    private bool _shaking = false;
+
+    private void Awake()
+    {
+        _noise = _vCam.GetCinemachineComponent<Cinemachine.CinemachineBasicMultiChannelPerlin>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+            StartShake();
+    }
+
+    private void LateUpdate()
+    {
+        if (_shaking)
+        {
+            //float offset = Mathf.Sin(_shakeFreq * Time.deltaTime) * _shakeAmp;
+            //_offset.m_Offset = new Vector3(offset, offset, 0);
+            _noise.m_AmplitudeGain = _shakeAmp;
+            _noise.m_FrequencyGain = _shakeFreq;
+        }
+    }
+
+    public void StartShake()
+    {
+        StartCoroutine(ShakeRoutine());
+    }
+
+    private IEnumerator ShakeRoutine()
+    {
+        _shaking = true;
+        yield return new WaitForSeconds(.2f);
+        _shaking = false;
+        _noise.m_AmplitudeGain = 0;
+        _noise.m_FrequencyGain = 0;
+        //_offset.m_Offset = Vector3.zero;
+    }
+}
